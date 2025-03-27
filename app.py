5e709c2a625b118e7f046e985b25f8969c4f87f3
Gorachand22 <babuldashbabul@gmail.com>
2025-03-27 13:55:05 +0530

    ops remove

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
 import streamlit as st
 import pickle
-from ops import transform_text
 import time
+import nltk
+
+# Ensure necessary NLTK resources are available before execution
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+def transform_text(text):
+    # Initialize the stemmer
+    ps = PorterStemmer()
+    stop_words = set(stopwords.words('english'))  # Convert to set for faster lookups
+    # Lowercasing
+    text = text.lower()
+
+    # Tokenization
+    tokens = word_tokenize(text)
+
+    # Removing special characters, stop words, and punctuation + Stemming
+    processed_tokens = [ps.stem(word) for word in tokens if word.isalnum() and word not in stop_words]
+
+    # Return the cleaned and transformed text
+    return " ".join(processed_tokens)
 
 # Define custom menu items
 custom_menu_items = {
